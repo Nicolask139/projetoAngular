f49b9a69069256d976021ca4fcbd95f54172335a
Nicolask139 <Nicolaskiy@gmail.com>
2024-09-29 21:53:27 -0300

    Valores variaveis

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,7 @@
+import { DUMMY_USERS } from '../dummy-users';
 import { Component } from '@angular/core';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
 @Component({
   selector: 'app-user',
@@ -7,5 +10,5 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-
+   selectedUser = DUMMY_USERS[randomIndex]
 }
